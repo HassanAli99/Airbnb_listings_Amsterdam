d2798e83b8965612ad1aa141479cc621e84a6b8e
Hassan Ali <hassanyare99@outlook.com>
2023-03-31 11:43:58 +0200

    Added airbnb listings.py

--- apps/listings.py
+++ apps/listings.py
@@ -8,7 +8,7 @@ def app():
     st.title("Airbnb Listings Map")
 
     # Load Airbnb data with latitude, longitude, name, and price
-    data_url = "https://raw.githubusercontent.com//HassanAli99//Airbnb_listings_Amsterdam//master/apps//data//listings_clean.csv"
+    data_url = "https://raw.githubusercontent.com/HassanAli99/Airbnb_listings_Amsterdam/master/apps/data/listings_clean.csv"
     data = pd.read_csv(data_url,  encoding="ISO-8859-1")
     # Create a Leafmap with OpenStreetMap tiles
     m = leafmap.Map(tiles="OpenStreetMap")
